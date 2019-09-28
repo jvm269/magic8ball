483278175d98e9ef11d50aa29c3c7d978b31ee5a
Blake M <drperson1@gmail.com>
2019-09-27 23:59:29 -0400

    updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # requests are objects that flask handles (get set post, etc)
 from flask import Flask, render_template, request
 # scientific computing library for saving, reading, and resizing images
-from scipy.misc import imread, imresize
+import PIL
+from PIL import Image
 # for matrix math
 import numpy as np
+import keras.models
 # for regular expressions, saves time dealing with string data
 import re
+import io
+from io import BytesIO
 import base64
 
 # system level operations (like loading files)
@@ -15,6 +19,7 @@ import os
 # tell our app where our saved model is
 sys.path.append(os.path.abspath("./model"))
 from load import *
+#from keras.models import model_from_json
 
 # initalize our flask app
 app = Flask(__name__)
@@ -27,11 +32,6 @@ model, graph = init()
 def index():
     return render_template("index.html")
 
-# decoding an image from base64 into raw representation
-def convertImage(imgData1):
-    imgstr = re.search(r'base64,(.*)', str(imgData1)).group(1)
-    with open('output.png', 'wb') as output:
-        output.write(base64.b64decode(imgstr))
 
 @app.route('/predict/', methods=['GET', 'POST'])
 def predict():
@@ -39,25 +39,24 @@ def predict():
     # to input the user drawn character as an image into the model
     # perform inference, and return the classification
     # get the raw data format of the image
+    # NEED IF THEN OR KNOWS POST OR GET
     imgData = request.get_data()
-    # encode it into a suitable format
-    convertImage(imgData)
+    # decoding an image from base64 into raw representation
+    imgstr = re.search(r'base64,(.*)', str(imgData)).group(1)
+    image_bytes = io.BytesIO(base64.b64decode(imgstr))
+    
     # read the image into memory
-    # use .convert('L') to convert file to (8-bit pixels, black and white) - gray scale
-    x = imread('output.png', mode='L')
-    # x = np.invert(x)
+    image = Image.open(image_bytes)
     # resize width & height to 28 x 28
-    x = imresize(x, (28, 28))
-    # DO I NEED TO CONVERT TO PIXELS?
-    img_array = np.array(x).astype(np.float32)
-    # img_array = img_array.reshape(784)
-    # imsave('final_image.jpg', x)
-    # convert to a 4D tensor to feed into our model
-    x = img_array.reshape(1, 28, 28, 1)
+    img_size = 28, 28
+    image = image.resize(img_size, Image.ANTIALIAS)
+    image = image.convert('1')
+    img_array = np.asarray(image)
+   
     # in our computation graph
     with graph.as_default():
         # perform the prediction
-        prediction = model.predict(x)
+        prediction = model.predict(img_array)
         print(prediction)
         # use np.argmax to find prediction value
         print(np.argmax(prediction, axis=1))

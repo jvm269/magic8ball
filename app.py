821fac76a2df5f247d62c7a50be4f2356c63a625
Blake M <drperson1@gmail.com>
2019-09-28 12:38:01 -0400

    test fetch

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ import base64
 import sys
 import os
 
-sys.path.append(os.path.abspath("./model"))
-from load import *
+#sys.path.append(os.path.abspath("./model"))
+#from load import *
 
 
 # Initalize our flask app
 app = Flask(__name__)
-global model, graph
-model, graph = init()
+#global model, graph
+#model, graph = init()
 
 # Route to render home page
 @app.route('/')
@@ -26,33 +26,33 @@ def index():
     return render_template("index.html")
 
 # Prediction route
-@app.route('/predict/', methods=['GET', 'POST'])
+@app.route('/predict/', methods=['POST'])
 def predict():
     # NEED IF THEN OR KNOWS POST OR GET
-    imgData = request.get_data()
+    image_data = request.get_data()
 
     # Convert base64 to image
-    imgstr = re.search(r'base64,(.*)', str(imgData)).group(1)
+    imgstr = re.search(r'base64,(.*)', str(image_data)).group(1)
     image_bytes = io.BytesIO(base64.b64decode(imgstr))
     
     # Convert image to pixels
     image = Image.open(image_bytes)
-    img_size = 28, 28
-    image = image.resize(img_size, Image.ANTIALIAS)
+    image_size = 28, 28
+    image = image.resize(image_size, Image.ANTIALIAS)
     image = image.convert('1')
-    img_array = np.asarray(image)
+    image_array = np.asarray(image)
    
     # Perform prediction and return response
     with graph.as_default():
-        prediction = model.predict(img_array)
+        prediction = model.predict(image_array)
         print(prediction)
         print(np.argmax(prediction, axis=1))
-        response = np.argmax(prediction, axis=1)
+        result = np.argmax(prediction, axis=1)
         # DO I NEED TO JSONIFY STRING?
         # return jsonify(str(response[0]))
-        return str(response[0])
+        return jsonify(str(result[0]))
 
-# Run locally
+# Run locally on port 5000
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 

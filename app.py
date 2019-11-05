04e21686966119511a8f811371004a0ba7529923
GanJin <gj2297@columbia.edu>
2019-11-04 21:44:18 -0500

    update app.py

--- app.py
+++ app.py
@@ -16,14 +16,11 @@ from tensorflow.keras.models import load_model
 app = Flask(__name__)
 
 
-def init():
-   global model, graph
-   # load the pre-trained Keras model
-   model = load_model("modellabeledfinal.h5")
-   graph = tf.compat.v1.get_default_graph()
-
-
-init()
+graph = tf.Graph()
+with graph.as_default():
+       session = tf.Session()
+       with session.as_default():
+          model = load_model('modellabeledfinal.h5')
 
 
 @app.route("/")
@@ -48,21 +45,20 @@ def predict():
     # print(image_array)
 
     # model = load_model("modellabeledfinal.h5")
-    prediction = model.predict(image_array)
-    print(prediction)
-    result = np.argmax(prediction, axis=1)
-    print(result)
+    with graph.as_default():
+        with session.as_default():
+            prediction = model.predict(image_array)
+            print(prediction)
+            result = np.argmax(prediction, axis=1)
+            print(result)
 
-    mapping =  {"0" : "0","1": "1","2": "2","3": "3","4": "4","5": "5","6": "6","7": "7","8": "8","9": "9","10": 'A',"11": 'B',"12": 'C',"13": 'D',"14": 'E',"15": 'F',"16": 'G',"17": 'H',"18": 'I',"19": 'J',"20": 'K',"21": 'L',"22": 'M',"23": 'N',"24": 'O',"25": 'P',"26": 'Q',"27": 'R',"28": 'S',"29": 'T',"30": 'U',"31": 'V',"32": 'W',"33": 'X',"34": 'Y',"35": 'Z'}
-    print(mapping[str(result[0])])
+            mapping =  {"0" : "0","1": "1","2": "2","3": "3","4": "4","5": "5","6": "6","7": "7","8": "8","9": "9","10": 'A',"11": 'B',"12": 'C',"13": 'D',"14": 'E',"15": 'F',"16": 'G',"17": 'H',"18": 'I',"19": 'J',"20": 'K',"21": 'L',"22": 'M',"23": 'N',"24": 'O',"25": 'P',"26": 'Q',"27": 'R',"28": 'S',"29": 'T',"30": 'U',"31": 'V',"32": 'W',"33": 'X',"34": 'Y',"35": 'Z'}
+            print(mapping[str(result[0])])
 
     return jsonify(mapping[str(result[0])])
 
 
 
-# if __name__ == "__main__":
-#     app.run(debug=False)
 if __name__ == "__main__":
-   print(("* Loading Keras model and Flask starting server..."
-       "please wait until server has fully started"))
-   app.run(debug=False)
+    # init()
+    app.run(host='0.0.0.0', port=5000, debug=False)
